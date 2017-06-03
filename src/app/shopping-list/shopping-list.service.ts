29a73ead40a2713244276f8e2393688e1b1ff470
Perry Anderson <perry.anderson@gmail.com>
2017-06-03 14:37:25 -0300

    Tweak on the ShoppingListService and base HTML for RecipeEdit.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -33,7 +33,7 @@ export class ShoppingListService {
   }
 
   updateIngredient(index: number, newIngredient: Ingredient) {
-    this.ingredients[ index ] = newIngredient; 
+    this.ingredients[ index ] = newIngredient;
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 
