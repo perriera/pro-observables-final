07d28bf500ade8735fbefb4bad711bebdc838c9f
Perry Anderson <perry.anderson@gmail.com>
2017-06-04 20:56:41 -0300

    Added functionalilty to add or update a Recipe.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -3,9 +3,12 @@ import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class RecipeService {
+
+  recipesChanged = new Subject<Recipe []>();
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -25,17 +28,29 @@ export class RecipeService {
       ])
   ];
 
-  constructor(private slService: ShoppingListService) {}
+  constructor(private slService: ShoppingListService) {
+  }
 
   getRecipes() {
     return this.recipes.slice();
   }
 
   getRecipe(index: number) {
-    return this.recipes[index];
+    return this.recipes[ index ];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
   }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice())
+  }
+
+  updateRecipe(index: number, recipe: Recipe) {
+    this.recipes[ index ] = recipe;
+    this.recipesChanged.next(this.recipes.slice())
+  }
+
 }
